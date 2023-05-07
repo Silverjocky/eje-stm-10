536a9440bf3df4b6ab7055cacb38c643a518191a
Salvador Trejo <silverdeath86@gmail.com>
2023-05-07 16:54:43 -0600

    Calculadora con js

---
+++ public/js/calculadora.js
@@ -0,0 +1,32 @@
+//Calculadora
+function sumaNumeros(){
+    //Declaracion let y const 
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
+    //Proceso
+    const res = n1 + n2;
+    //Salida
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado suma: ' + res);
+}
+function restaNumeros(){
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
+    const res = n1 - n2;
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado:' + res);
+}
+function multiNumeros(){
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
+    const res = n1 * n2;
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado:' + res);
+}
+function divNumeros(){
+    const n1=parseFloat(document.getElementById('ent1').value);
+    const n2=parseFloat(document.getElementById('ent2').value);
+    const res = n1 / n2;
+    document.getElementById('resultado').innerHTML= 'Resultado: '+res;
+    //alert('Resultado:' + res);
+}
